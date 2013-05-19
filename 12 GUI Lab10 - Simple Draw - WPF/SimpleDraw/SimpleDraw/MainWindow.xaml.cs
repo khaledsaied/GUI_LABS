d2de33153d22a2370d912c56ad6d6e9f859f26b1
khaledsaied <kaledsaied@gmail.com>
2013-05-19 15:04:01 +0200

    school stuff...

---
+++ 12 GUI Lab10 - Simple Draw - WPF/SimpleDraw/SimpleDraw/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleDraw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeSizeEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyboardDevice.IsKeyDown(Key.Enter))
+            {    
+                Shape r = e.Source as Shape;
+                //Ellipse r = (Ellipse)e.Source;
+                if (r != null)
+                {
+                    r.Width += 10;
+                }
+            }          
+        }
+    }
+}
